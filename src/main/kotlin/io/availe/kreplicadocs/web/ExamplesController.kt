a520c01f86d04c570d9e9f2bde9f4cb988b55e6a
availe <94802525+availe@users.noreply.github.com>
2025-07-20 16:38:25 -0400

    fix updated imports

--- src/main/kotlin/io/availe/kreplicadocs/web/ExamplesController.kt
+++ src/main/kotlin/io/availe/kreplicadocs/web/ExamplesController.kt
@@ -1,18 +1,15 @@
 package io.availe.kreplicadocs.web
 
 import io.availe.kreplicadocs.common.FragmentTemplate
-import io.availe.kreplicadocs.common.PartialTemplate
 import io.availe.kreplicadocs.common.WebApp
-import io.availe.kreplicadocs.model.Example
-import io.availe.kreplicadocs.model.ExampleSlug
-import io.availe.kreplicadocs.model.FileName
-import io.availe.kreplicadocs.model.toViewModel
+import io.availe.kreplicadocs.model.*
 import io.availe.kreplicadocs.services.ExampleDataProvider
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.servlet.view.FragmentsRendering
 
 @Controller
 class ExamplesController(private val provider: ExampleDataProvider) {
@@ -23,13 +20,23 @@ class ExamplesController(private val provider: ExampleDataProvider) {
 
     @HxRequest
     @GetMapping(WebApp.Endpoints.Examples.PLAYGROUND)
-    fun getExamplePlayground(@PathVariable slug: ExampleSlug, model: Model): String {
-        return withExample(slug) { example ->
-            model.addAttribute(WebApp.ViewModelAttributes.EXAMPLE, example)
-            model.addAttribute(WebApp.ViewModelAttributes.ALL_EXAMPLES, provider.getAllExamples())
-            model.addAttribute(WebApp.ViewModelAttributes.ACTIVE_SLUG, slug.value)
-            PartialTemplate.EXAMPLE_PLAYGROUND_UPDATE.path
+    fun getExamplePlayground(@PathVariable slug: ExampleSlug, model: Model): FragmentsRendering {
+        val example = provider.getExampleBySlug(slug)
+            ?: return FragmentsRendering.with(FragmentTemplate.EXAMPLE_NOT_FOUND.path).build()
+
+        val allExamples = provider.getAllExamples()
+        val exampleSelectOptions = allExamples.map {
+            SelectOption(it.slug, it.name, it.slug == example.slug)
         }
+
+        model.addAttribute(WebApp.ViewModelAttributes.EXAMPLE, example)
+        model.addAttribute(WebApp.ViewModelAttributes.ALL_EXAMPLES, allExamples)
+        model.addAttribute(WebApp.ViewModelAttributes.ACTIVE_SLUG, slug.value)
+        model.addAttribute(WebApp.ViewModelAttributes.EXAMPLE_SELECT_OPTIONS, exampleSelectOptions)
+
+        return FragmentsRendering.with("fragments/playground-main")
+            .fragment("fragments/examples-sidebar-links")
+            .build()
     }
 
     @HxRequest
