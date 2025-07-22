9ebfebceaf0e345ae28a42d9b6f7510530cf7ec6
availe <94802525+availe@users.noreply.github.com>
2025-07-22 16:07:52 -0400

    changes

--- src/main/resources/static/js/playground.js
+++ src/main/resources/static/js/playground.js
@@ -18,17 +18,27 @@
 
                     const hiddenTextarea = document.querySelector('textarea[name="source"]');
                     const initialCode = hiddenTextarea ? hiddenTextarea.value : '';
+                    const editorNode = document.getElementById('kreplica-editor');
 
                     window.kreplicaEditor = monaco.editor.create(
-                        document.getElementById('kreplica-editor'),
+                        editorNode,
                         {
                             value: initialCode,
                             language: 'kotlin',
                             automaticLayout: true,
                             theme: 'vs-dark',
-                            minimap: {enabled: false}
+                            minimap: {enabled: false},
+                            folding: true,
+                            scrollBeyondLastLine: false
                         }
                     );
+
+                    const updateEditorHeight = () => {
+                        if (window.kreplicaEditor) {
+                            const contentHeight = window.kreplicaEditor.getContentHeight();
+                            editorNode.style.height = `${contentHeight}px`;
+                        }
+                    };
 
                     monaco.languages.registerCompletionItemProvider('kotlin', {
                         triggerCharacters: ['@', '.'],
@@ -37,11 +47,14 @@
                         }
                     });
 
-                    if (hiddenTextarea) {
-                        window.kreplicaEditor.onDidChangeModelContent(function () {
+                    window.kreplicaEditor.onDidChangeModelContent(() => {
+                        if (hiddenTextarea) {
                             hiddenTextarea.value = window.kreplicaEditor.getValue();
-                        });
-                    }
+                        }
+                        updateEditorHeight();
+                    });
+
+                    updateEditorHeight();
                 });
         });
     };
